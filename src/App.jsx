d2d175d8672058319b9ecc820b6edaac794bdcae
SenpuDev <senpuudev@gmail.com>
2023-08-02 11:42:02 +0200

    gh-pages router testing

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, createHashRouter, RouterProvider } from 'react-router-dom'
 import AuthLayout from './layouts/AuthLayout'
 import Login from './pages/Login'
 import SignUp from './pages/SignUp'
@@ -14,31 +14,48 @@ import Project from './components/Project'
 import EditProject from './pages/EditProject'
 import NewCollaborator from './pages/NewCollaborator'
 
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <AuthLayout />,
+    children: [
+      { index: true, element: <Login /> }
+    ]
+  },
+  {
+    path: '/projects',
+    element: <PrivateAreaLayout />
+  }
+])
+
 function App () {
   return (
     <>
-      <BrowserRouter>
-        <AuthProvider>
-          <ProjectsProvider>
-            <Routes>
-              <Route path='/' element={<AuthLayout />}>
-                <Route index element={<Login />} />
-                <Route path='sign-up' element={<SignUp />} />
-                <Route path='forgot-password' element={<ForgotPassword />} />
-                <Route path='reset-password/:token' element={<ResetPassword />} />
-                <Route path='verify/:token' element={<VerifyAccount />} />
-              </Route>
-              <Route path='/projects' element={<PrivateAreaLayout />}>
-                <Route index element={<Projects />} />
-                <Route path='new-project' element={<NewProject />} />
-                <Route path='new-collaborator/:id' element={<NewCollaborator />} />
-                <Route path=':id' element={<Project />} />
-                <Route path='edit/:id' element={<EditProject />} />
-              </Route>
-            </Routes>
-          </ProjectsProvider>
-        </AuthProvider>
-      </BrowserRouter>
+      <RouterProvider router={router}>
+
+        <BrowserRouter>
+          <AuthProvider>
+            <ProjectsProvider>
+              <Routes>
+                <Route path='/' element={<AuthLayout />}>
+                  <Route index element={<Login />} />
+                  <Route path='sign-up' element={<SignUp />} />
+                  <Route path='forgot-password' element={<ForgotPassword />} />
+                  <Route path='reset-password/:token' element={<ResetPassword />} />
+                  <Route path='verify/:token' element={<VerifyAccount />} />
+                </Route>
+                <Route path='/projects' element={<PrivateAreaLayout />}>
+                  <Route index element={<Projects />} />
+                  <Route path='new-project' element={<NewProject />} />
+                  <Route path='new-collaborator/:id' element={<NewCollaborator />} />
+                  <Route path=':id' element={<Project />} />
+                  <Route path='edit/:id' element={<EditProject />} />
+                </Route>
+              </Routes>
+            </ProjectsProvider>
+          </AuthProvider>
+        </BrowserRouter>
+      </RouterProvider>
     </>
   )
 }
