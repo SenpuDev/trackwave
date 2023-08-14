23260f23b1f19624b0d0bc0ee4f8e0a892e4d6fd
SenpuDev <senpuudev@gmail.com>
2023-08-14 13:18:16 +0200

    User name color change

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -15,7 +15,9 @@ const Sidebar = () => {
   return (
     <div className='relative'>
       <aside className='p-10 m-10 flex shadow-md gap-4 flex-col bg-gray-50 dark:bg-slate-800 rounded sticky top-0'>
-        <p className='text-xl dark:text-gray-50'>Wellcome: {auth.name}</p>
+        <p className='text-xl dark:text-gray-50'>Wellcome:
+          <span className='text-orange-400'> {auth.name}</span>
+        </p>
         <Link className='bg-indigo-400 hover:bg-indigo-500 transition-all py-1 px-2 cursor-pointer text-center text-white font-bold rounded uppercase' to='new-project'>New Project</Link>
         <button className='bg-indigo-400 hover:bg-indigo-500 transition-all py-1 px-2 cursor-pointer text-white font-bold rounded uppercase' type='button' onClick={handleClick}>Log out</button>
       </aside>
